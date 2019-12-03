fe06a6ad7d357e2157d7097f67a7d7ffbb0acc3d
cetv <ctiszav@gmail.com>
2019-12-03 02:40:33 -0500

    sec

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyCs2u4gt_JqIuLhruy4cFZyZxtAK2Tivzk",
-    authDomain: "fb001-a7c9c.firebaseapp.com",
-    databaseURL: "https://fb001-a7c9c.firebaseio.com",
-    projectId: "fb001-a7c9c",
+    apiKey: "xxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxx",
+    databaseURL: "xxxxxxxxxxxxxxxxx",
+    projectId: "xxxxxxxxxx",
     storageBucket: "",
-    messagingSenderId: "899134063293"
+    messagingSenderId: "xxxxxxx"
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,12 +6,12 @@ export const environment = {
   production: false,
 
   firebase: {
-    apiKey: "AIzaSyCs2u4gt_JqIuLhruy4cFZyZxtAK2Tivzk",
-    authDomain: "fb001-a7c9c.firebaseapp.com",
-    databaseURL: "https://fb001-a7c9c.firebaseio.com",
-    projectId: "fb001-a7c9c",
+    apiKey: "xxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxx",
+    databaseURL: "xxxxxxxxxxxxxxxxx",
+    projectId: "xxxxxxxxxx",
     storageBucket: "",
-    messagingSenderId: "899134063293"
+    messagingSenderId: "xxxxxxx"
   }
 };
 
